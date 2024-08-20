f0008504017f8fcc86247a2ecc5c3da643f5cef3
MaksimObozniy <125642497+MaksimObozniy@users.noreply.github.com>
2024-08-20 14:18:41 +0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,92 @@
+import os
+import json
+import folium
+import requests
+from flask import Flask
+from pprint import pprint
+from geopy import distance
+
+
+API_KEY = os.environ['API_KEY']
+
+
+def fetch_coordinates(apikey, address):
+  base_url = "https://geocode-maps.yandex.ru/1.x"
+  response = requests.get(base_url,
+                          params={
+                              "geocode": address,
+                              "apikey": apikey,
+                              "format": "json",
+                          })
+  response.raise_for_status()
+  found_places = response.json(
+  )['response']['GeoObjectCollection']['featureMember']
+
+  if not found_places:
+    return None
+
+  most_relevant = found_places[0]
+  lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+  return lat, lon
+
+
+def main():
+    with open("coffee.json", "r", encoding="CP1251") as my_file:
+      file_contents = my_file.read()
+      coffee_name = json.loads(file_contents)
+
+    user_location = input("Где вы находитесь? ")
+    coords = fetch_coordinates(API_KEY, user_location)
+    print("Ваши координаты:", coords)
+
+    coffee_dict = []
+
+    for cofee in coffee_name:
+      title = cofee['Name']
+      latitude = cofee['geoData']['coordinates'][1]
+      langtitude = cofee['geoData']['coordinates'][0]
+      coordination_caffee = (latitude, langtitude)
+      dist = distance.distance(coords, coordination_caffee).miles
+      coffee_list = {
+          'title': title,
+          'distance': dist,
+          'latitude': latitude,
+          'longtitude': langtitude
+      }
+      
+      coffee_dict.append(coffee_list)
+
+  
+    def get_name_distance(a):
+      return a['distance']
+
+  
+    b = sorted(coffee_dict, key=get_name_distance)
+    pprint(b[:5])
+
+    m = folium.Map(location=coords, zoom_start=12)
+
+  
+    for i in range(5):
+      folium.Marker(
+          location=[b[i]['latitude'], b[i]['longtitude']],
+          tooltip="Click me!",
+          popup=b[i]['title'],
+          icon=folium.Icon(icon="cloud"),
+      ).add_to(m)
+  
+    m.save("index.html")
+
+
+    def hello_world():
+        with open('index.html') as file:
+          return file.read()
+
+  
+    app = Flask(__name__)
+    app.add_url_rule('/', 'hello', hello_world)
+    app.run('0.0.0.0')
+
+
+if __name__ == "__main__":
+  main()
